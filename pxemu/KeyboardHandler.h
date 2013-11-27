b4ee41697003cd3270946342356a1c89accfecca
trieck <trieck@gmail.com>
2013-11-26 21:16:51 -0500

    Incremental

--- pxemu/KeyboardHandler.h
+++ pxemu/KeyboardHandler.h
@@ -8,6 +8,8 @@
 #ifndef __KEYBOARD_HANDLER_H__
 #define __KEYBOARD_HANDLER_H__
 
+#include "KeyboadMatrix.h"
+
 /////////////////////////////////////////////////////////////////////////////
 template <class T>
 class KeyboardHandler
@@ -20,15 +22,24 @@ public:
 	// Interface
 	BEGIN_MSG_MAP(KeyboardHandler)
 		MESSAGE_HANDLER(WM_KEYDOWN, OnKeyDown)
+		MESSAGE_HANDLER(WM_KEYUP, OnKeyUp)
 	END_MSG_MAP()
 
 	// Implementation
 private:
 	LRESULT OnKeyDown(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
 	{
+		uint8_t result = KeyboardMatrix::translate(wParam, lParam);
+
 		return 0;
 	}
 
+	LRESULT OnKeyUp(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		uint8_t result = KeyboardMatrix::translate(wParam, lParam);
+
+		return 0;
+	}
 };
 
 #endif // __KEYBOARD_HANDLER_H__
